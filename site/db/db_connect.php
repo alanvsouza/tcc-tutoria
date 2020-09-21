52ae1c1897b6eb476002364547479f91e05d4e81
Victor Hugo Costa Osses <victor.costa.osses@gmail.com>
2020-09-21 16:57:54 -0300

    Atualizando o site

--- site/db/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function connect($hostname, $dbname, $username, $password) {
-    $conn = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    return $conn;
-}
