301bbb01dbf32f4e08fc0c4cd8fffdefc6609ccb
Victor Hugo Costa Osses <victor.costa.osses@gmail.com>
2020-10-22 00:50:44 -0300

    Adicionando novo HTML para os cards de eventos

--- site/App/Model/EventoService.php
+++ site/App/Model/EventoService.php
@@ -4,6 +4,20 @@ namespace App\Model;
 
 class EventoService {
     public static function renderizarEventos() {
+        $meses = array(
+            "01"=>"JAN",
+            "02"=>"FEV",
+            "03"=>"MAR",
+            "04"=>"ABR",
+            "05"=>"MAI",
+            "06"=>"JUN",
+            "07"=>"JUL",
+            "08"=>"AGO",
+            "09"=>"SET",
+            "10"=>"OUT",
+            "11"=>"NOV",
+            "12"=>"DEZ",);
+
         $eventoDao = new EventoDAO();
         
         $eventos = $eventoDao->readAllFromToday();
@@ -13,11 +27,14 @@ class EventoService {
                 $row = $eventos[$i];
 
                 $data = (str_replace('-','/', $row['dataevento']));
+                $dataSplit = explode("/",$data);
 
-                echo '
+                echo'
                 <div class="post">
                 <div class="header_post">
                     <img src="img/'.$row['image'].'" alt="img-card">
+                    <div id="data">'.$dataSplit[2].'<br> '.$meses[$dataSplit[1]].'</div>
+                    <div id="triangulo-para-baixo"></div>
                 </div>
 
                 <div class="body_post">
