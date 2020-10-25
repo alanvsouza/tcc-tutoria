7a5722667e6ad738f7e98aa6777223bf37176d25
Victor Hugo Costa Osses <victor.costa.osses@gmail.com>
2020-10-25 15:13:42 -0300

    Atualizando o login

--- site/login.php
+++ site/login.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>Login 2</title>
+    <title>Login - Cotil Amigável</title>
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
@@ -15,7 +15,7 @@ session_start();
     <form action="functions/login.php" method="post">
         <div id="header">
             <span>Sistema de Login</span>
-            <p id="nome_site"><a href="index.php">Cotil Amigável</a></p>
+            <p><a href="index.php">Cotil Amigável</a></p>
         </div>
         <?php
         if (isset($_SESSION['nao_autenticado'])):
@@ -50,11 +50,11 @@ session_start();
         unset($_SESSION['cadastro_sucesso']);
         ?>
         <div id="body">
-            <input type="text" name="usuario" id="usuario" placeholder="Informe seu usuário">
-            <input type="password" name="senha" id="senha" placeholder="Informe sua senha" maxlength="70" minlength="1">
+            <input type="text" name="usuario" id="usuario" placeholder="Informe seu usuário" class="hoverEfect">
+            <input type="password" name="senha" id="senha" placeholder="Informe sua senha" class="hoverEfect" maxlength="70" minlength="1">
             <p class="esqueceu-senha"><a href="#">Esqueci minha senha!</a></p>
             <input type="submit" id="btn-entrar" value="Entrar">
-            <p id="cadastre_se">Ainda não possui uma conta?<a href="cadastro.php"> Cadastre-se aqui!</a></p>
+            <p id="cadastre_se">Ainda não possui uma conta?<br><a href="cadastro.php"> Cadastre-se aqui!</a></p>
         </div>
     </form>
     
