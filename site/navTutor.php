8f25731504313a32e23360e14f44a444704e2cc8
Victor Hugo Costa osses <victor.costa.osses@gmail.com>
2020-11-16 00:48:31 -0300

    Atualizando arquivos do site

--- site/navTutor.php
+++ site/navTutor.php
@@ -12,7 +12,8 @@ require_once "vendor/autoload.php";
             <?php $basename = basename($_SERVER['PHP_SELF'], '.php'); ?>
             <li><a <?php if ($basename == "index") echo "class = actived" ?> href="index.php">Eventos</a></li>
             <li><a <?php if ($basename == "projetoTutoria") echo "class = actived" ?> href="projetoTutoria.php">Projeto Tutoria</a></li>
-            <li><a <?php if ($basename == "horariosTutor") echo "class = actived" ?> href="horariosTutor.php">Horários</a></li>
+            <li><a <?php if ($basename == "horariosTutor") echo "class = actived" ?> href="horariosTutor.php">Tutor horários</a></li>
+            <li><a <?php if ($basename == "tutoriasAgendadas") echo "class = actived" ?> href="tutoriasAgendadas.php">Tutorias Agendadas</a></li>
             <?php
             if (!isset($_SESSION['usuario'])) {
                 echo
